59ed5742ca403ca1e36ca51271bc6b57bfe84f6e
abimaelrsergio <abimaelr.sergio@gmail.com>
2022-03-13 22:18:39 -0300

    commitando a calculadora

---
+++ calculadora/src/main/java/br/com/abimael/AbimaelApplication.java
@@ -0,0 +1,13 @@
+package br.com.abimael;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AbimaelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AbimaelApplication.class, args);
+	}
+
+}
