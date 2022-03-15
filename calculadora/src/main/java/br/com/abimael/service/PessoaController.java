52a769da650b157db5117c1ecc5d1999e114f111
abimaelrsergio <abimaelr.sergio@gmail.com>
2022-03-15 08:45:54 -0300

    Novas clases

---
+++ calculadora/src/main/java/br/com/abimael/service/PessoaController.java
@@ -0,0 +1,31 @@
+package br.com.abimael.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.abimael.model.Pessoa;
+
+@RestController
+@RequestMapping("/pessoas")
+public class PessoaController {
+
+	@Autowired
+	private PessoaService pessoaService;
+
+	@GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+	public Pessoa findById(@PathVariable("id") String id) {
+		return pessoaService.findById(id);
+	}
+
+	@GetMapping
+	public List<Pessoa> findAll() {
+		return pessoaService.findAll();
+	}
+
+}
