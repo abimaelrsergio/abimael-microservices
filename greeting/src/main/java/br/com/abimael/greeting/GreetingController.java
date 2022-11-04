2f0b443660776f81ca7669e25e740b67c962d1dc
Abimael Sergio <abimaelr.sergio@gmail.com>
2022-11-04 09:29:06 -0300

    update

--- greeting/src/main/java/br/com/abimael/greeting/GreetingController.java
+++ greeting/src/main/java/br/com/abimael/greeting/GreetingController.java
@@ -2,6 +2,8 @@ package br.com.abimael.greeting;
 
 import java.util.concurrent.atomic.AtomicLong;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,5 +17,10 @@ public class GreetingController {
 	@RequestMapping("/greeting")
 	public Greeting greeting(@RequestParam(name = "name", defaultValue = "World") String name) {
 		return new Greeting(counter.incrementAndGet(), String.format(template, name));
+	}
+
+	@GetMapping("/calcular/numberOne/numberTwo")
+	public Double calculador(@PathVariable("numberOne") String numberOne, @PathVariable("numberTwo") String numberTwo) {
+		return 1d;
 	}
 }
